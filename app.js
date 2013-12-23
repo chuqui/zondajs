8d1d297948e2e8744c4575c4974752feba6c00b8
Humberto Miatello <hmiatello@rupux.com>
2013-12-23 08:42:18 -0300

    DI and routing

---
+++ app.js
@@ -0,0 +1,40 @@
+// ignore this file, it's only used for quick concept testing
+// this file will get deleted
+
+var app = require('./zondajs');
+
+console.log("Application started");
+
+app.component('mensaje', {
+    hola: 'hola',
+    otro: 'mundo'
+});
+
+app.component('messageService', function(){
+    return {
+        print: function(){
+            console.log('---- service ----');
+        }
+    };
+});
+
+app.controller.get('/', function(request, response, messageService, mensaje){
+    console.log('in the controller /');
+    messageService.print();
+    response.end("hola juan");
+});
+
+app.controller.get('/hola', function(request, response, messageService, mensaje){
+    console.log('in the controller /');
+    messageService.print();
+});
+
+app.controller.get('/hola/:hola/:name', function(request, response, messageService, mensaje){
+    console.log('in the controller /');
+    messageService.print();
+    console.log(request.params.hola + " " + request.params.name);
+});
+
+
+app.startApp(8080);
+
--- zondajs.js
+++ zondajs.js
@@ -1,20 +1,149 @@
-var __zonda = {
-	// dependency injection
-	dependencyMap: [],
-	injectAndCall: function(callable){},
-	
-	// arquitecture util
-	object: function(name, o){},
-	component: function(name, callable){},
-	service: function(name, callable){},
-	controller: function(route, opts, callable){},
-	filter: function(o){},
+var http    = require("http");
+var _       = require("underscore");
+var url     = require("url");
 
-	// internal use
-	internalData: {},
-	route: function(uri){}	
-}
-exports.createZondaApp = function(opts){
-	// parse app level options
-	return __zondajs;
-};
+
+(function(){
+
+    var zondajs = {
+        __routes:{
+            list: [],
+            config: {
+                namedRegex: /:\w+/g
+            },
+            add: function(path, method, callable){
+                zondajs.__routes.list.push({
+                    path: path,
+                    regex: '^' + path.replace(zondajs.__routes.config.namedRegex, '([^\/]+)') + '$',
+                    method: method,
+                    callable: callable
+                });
+            },
+            get: function(path, method){
+                var candidate = _.find(zondajs.__routes.list, function(route){
+                    rx = new RegExp(route.regex);
+                    return rx.test(path);
+                });
+
+                if(candidate != undefined){
+                    rx1 = new RegExp(zondajs.__routes.config.namedRegex);
+                    paramNames = candidate.path.match(rx1);
+                    paramValues = rx.exec(path);
+                    var params = {};
+                    _.each(paramNames, function(param, index){
+                        params[param.replace(':', '')] = paramValues[index + 1]
+                    });
+                    return {
+                        method: candidate.callable,
+                        params: params
+                    };
+                }else{
+                    return false;
+                }
+            }
+        },
+        __di : {
+            map: [],
+            get: function(key){
+                if(this[key]){
+                    return this.key;
+                }
+
+                var candidate =  _.find(zondajs.__di.map, function(entry){
+                    return (entry.key == key);
+                });
+
+                if(candidate != undefined){
+                    return candidate.value;
+                }else{
+                    return candidate;
+                };
+            },
+            identifyParams: function(callable){
+                var regexp = /^function\s*[^\( ]*\(\s*([^\)]*)\)/m;
+                var fnParams =  callable.toString().match(regexp)[1].replace(/\s/g, '').split(',');
+                
+                // clean request and response
+                return _.without(fnParams, 'request', 'response');
+            },
+            getDependencies: function(callable){
+                var params = [];
+                var keys = zondajs.__di.identifyParams(callable);
+                _.each(keys, function(key){
+                    var dependency = zondajs.__di.get(key);
+                    if(dependency!=undefined){
+                        if(dependency instanceof Function){
+                            params.push(dependency.apply(this, zondajs.__di.getDependencies(dependency)));
+                        }else{
+                            params.push(dependency);
+                        }
+                    }
+                });
+                return params;
+            },
+            invoke: function(callable, hooks){
+                if(hooks != undefined){
+                    callable.apply(this, _.union(hooks, zondajs.__di.getDependencies(callable)));
+                }else{
+                    callable.apply(this, zondajs.__di.getDependencies(callable));
+                }
+            }
+        },
+        properties: {
+            map:[],
+            set: function(key, value){
+                zondajs.properties.map.push({
+                    key: key,
+                    value: value
+                });
+            },
+            get: function(key){
+                // this will fail if the key isn't found
+                return _.find(zondajs.properties.map, function(entry){
+                    return (entry.key == key);
+                }).value;
+            }
+        },
+	    component:  function(name, object){
+            zondajs.__di.map.push({
+                key: name,
+                value: object
+            });    
+        },
+	    controller: {
+            get: function(path, callable){
+                zondajs.__routes.add(path, 'GET' ,callable);
+            },
+            post: function(path, callable){
+                zondajs.__routes.add(path, 'POST' ,callable);
+            }
+            //add support (at least) for rest http methods put and delete
+        },
+        startApp: function(port){
+            http.createServer(function(request, response) {
+                try{
+                    var parsedURL = url.parse(request.url, true);
+                    controller = zondajs.__routes.get(parsedURL.pathname, request.method);
+                    if(controller){
+                        request.params = _.defaults(controller.params, parsedURL.query);
+                        zondajs.__di.invoke(controller.method, [request, response]);
+                        response.writeHead(200, {"Content-Type": "text/plain"});
+                        response.write("200 OK");
+                        response.end();
+                    }else{
+                        response.writeHead(404, {"Content-Type": "text/plain"});
+                        response.write("File Not Found");
+                        response.end();
+                    }
+                }catch(e){
+                    console.log(e);
+                    response.writeHead(500, {"Content-Type": "text/plain"});
+                    response.write("Server error");
+                    response.end();
+                }
+            }).listen(port);
+        }
+    };
+
+    module.exports = zondajs;
+}).call(this);
